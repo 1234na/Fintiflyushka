83230123a6371486c844b7bbdde8e3e1e9404615
1234na <113606360+1234na@users.noreply.github.com>
2023-04-24 02:07:46 +0300

    Merge remote-tracking branch 'origin/master'

--- site_code.py
+++ site_code.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import request
+from flask import render_template
 import sqlite3
 
 app = Flask(__name__)
@@ -7,11 +8,21 @@ username = None
 
 
 @app.route('/')
-@app.route('/home/<username>')
-def home(username=None):
-    f = open('homepage.html', 'rb')
+def mainpage():
+    f = open('mainpage.html', 'rb')
     result = f.read()
     return result
+
+@app.route('/user/<name>')
+def userpage(name):
+    con = sqlite3.connect("fintiflyushka.sqlite")
+    cur = con.cursor()
+    about = cur.execute(f'''SELECT Extra FROM Users WHERE Username == "{username}"''').fetchall()
+    return render_template('user_cab.html', username=name, about=about)
+
+@app.route('/home/<name>')
+def home(name):
+    return render_template('homepage.html', username=name)
 
 @app.route('/enter', methods=['GET', 'POST'])
 def entrance():
@@ -28,11 +39,9 @@ def entrance():
         password_right = cur.execute(f'''SELECT Password FROM Users WHERE Username == "{username}"''').fetchall()
         try:
             if password_right[0][0] == password:
-                f = open('homepage.html', 'rb')
-                result = f.read()
-                return result
+                return render_template('homepage.html', username=username)
             else:
-                return 'Я сам в ахуе, но пшёл вон'
+                return 'Пользователь с таким именем не найден.'
         except BaseException:
             return 'Ошибка входа. Попробуйте перезайти на сайт.'
 
@@ -57,10 +66,8 @@ def registration():
         password_right = cur.execute(f'''SELECT Password FROM Users WHERE Username == "{username}"''').fetchall()
         if password_right == []:
             cur.execute(f'''INSERT INTO Users(Username,Password,Extra,Photos_amount) VALUES ("{username}", "{password}", "{about}", 0)''')
-            f = open('successful_reg.html', 'rb')
-            result = f.read()
             con.commit()
-            return result
+            return render_template('successful_reg.html', username=username)
         else:
             return 'Данный ник уже был зарегистрирован. Попробуйте войти в аккаунт, либо придумайте другой никнейм.'
 
